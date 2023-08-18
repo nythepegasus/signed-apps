b91d05f109baed889238ede0281921a9755ccf29
Nythepegasus <me@nythepegas.us>
2023-08-17 20:20:30 -0400

    Initial commit for SvelteKit rewrite

---
+++ src/components/App.svelte
@@ -0,0 +1,57 @@
+<script>
+	export let icon_src;
+	export let name;
+	export let author;
+	export let description;
+	export let download;
+	export let official;
+</script>
+
+<div id="app">
+	<img id="icon" src="{icon_src}" alt="{name} icon" />
+	<div id="name">{name}</div>
+	<div id="author">{author}</div>
+	<div id="description">{description}</div>
+	<button id="download" on:click={ () => {window.open(download, '_blank'); return false; } }>Download</button>
+	<button id="official" on:click={ () => {window.open(official, '_blank'); return false; } }>Official Page</button>
+</div> 
+
+<style>
+#app {
+  display: grid;
+	grid-template-columns: 10em 12em;
+	gap: 0.2em 0.8em;
+	margin: 0.1em 0em;
+	padding: 0.1em 0em;
+}
+#icon {
+	border-radius: 25px;
+	height: 10em;
+  width: 10em;
+	grid-column: 1;
+	grid-row: 1 / 5;
+}
+#name {
+  grid-column: 2;
+	grid-row: 1;
+	font-size: 1.3em;
+}
+#author {
+	grid-column: 2;
+	grid-row: 2;
+}
+#description {
+	grid-column: 2;
+	grid-row: 3;
+}
+#download {
+	grid-column: 1;
+	grid-row: 5;
+	font-size: 1.1em;
+}
+#official {
+	grid-column: 2;
+	grid-row: 5;
+	font-size: 1.1em;
+}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,62 @@
+<script>
+	import App from '../components/App.svelte';
+	const apps = [{
+		icon_src: "/sidestore.png",
+		name: "SideStore",
+		author: "SideStore Team",
+		description: "The home of jailed semi-untethered sideloading! This is the stable version.",
+		download: "itms-services://?action=download-manifest&url=https://b.sidestore.app/files/sidestore.stable.plist",
+		official: "https://sidestore.io/"
+	},
+	{
+		icon_src: "/sidestore.png",
+		name: "SideStore Nightly",
+		author: "SideStore Team",
+		description: "The home of jailed semi-untethered sideloading! This is the nightly version.",
+		download: "itms-services://?action=download-manifest&url=https://b.sidestore.app/files/sidestore.nightly.plist",
+		official: "https://sidestore.io/"
+	},
+	{
+		icon_src: "/esign.png",
+		name: "ESign",
+		author: "ESign Inc.",
+		description: "ESign is a free IPA signature tool.",
+		download: "itms-services://?action=download-manifest&url=https://b.sidestore.app/files/esign.plist",
+		official: "https://esign.yyyue.xyz/"
+	},
+	{
+		icon_src: "/jitterbug.png",
+		name: "Jitterbug",
+		author: "osy",
+		description: "An on-device JIT enabler.",
+		download: "itms-services://?action=download-manifest&url=https://b.sidestore.app/files/jitterbug.plist",
+		official: "https://github.com/osy/jitterbug"
+	}]
+	function startMusic(){
+		const soundEffect = new Audio();
+		soundEffect.autoplay = true;
+		soundEffect.src = "https://b.sidestore.app/wii.mp3";
+		soundEffect.title = "Wii Shop Theme - SideStore Premium";
+		soundEffect.play();
+	}
+
+</script>
+<div on:touchstart|once={startMusic}>
+	<p>Tap me for music :)</p>
+	<div id="apps">
+		{#each apps as app}
+			<App {...app} />
+		{/each}
+	</div>
+</div>
+
+<style>
+#apps {
+	display: grid;
+	grid-template-columns: repeat(auto-fit, minmax(22.4em, 1fr));
+	gap: 0.2em 0.3em;
+	align-items: center;
+	justify-content: center;
+	width: 100%;
+}
+</style>
